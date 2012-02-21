5eb7d005b3a2d2d562d80767bebbd64629bb87e8
Pavel <p.mashkov@gmail.com>
2012-02-22 00:48:22 +0400

    Выкладываю ДЗ. Эратосфен

---
+++ эратосфен/ConsoleApplication5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] iratosfen = new int[10000];
+
+            for (int i = 0; i < 10000; i++)
+            {
+                iratosfen[i] = i;
+//                Console.WriteLine(iratosfen[i]);
+            }
+
+            for (int i = 2; i < 10000; i++)
+            {
+                if (iratosfen[i] != 0)
+                {
+                    int j = i*i;
+                    while (j <= 9999)
+                    {
+                        iratosfen[j] = 0;
+                        j += i; 
+                    }
+                }
+            }
+
+            for (int i = 0; i < 10000; i++)
+            {
+                if (iratosfen[i] != 0) Console.Write(" " + iratosfen[i]);
+            }
+                Console.ReadKey();
+        }
+    }
+}
