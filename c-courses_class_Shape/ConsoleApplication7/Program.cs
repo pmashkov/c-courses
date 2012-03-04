e2015fd81aa8e7ef1c37976e882e1aaa31faf6ad
Pavel <p.mashkov@gmail.com>
2012-03-04 23:51:39 +0400

    new

---
+++ c-courses_class_Shape/ConsoleApplication7/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] showShape = new string[10];
+            Random randomShow = new Random();
+
+            //Shape shape = new Shape("red", 2, 2);
+            //Console.WriteLine(shape.color + " - цвет");
+
+            Square square = new Square("red", 2, 2, 4);
+            Console.WriteLine(square.lourge + " - длинна");
+
+            Circle circle = new Circle("red", 2, 2, 5);
+            Console.WriteLine(circle.radius + " - радус");
+
+
+            //Далее следует очень корявый цикл) Прошу больно бить ногами
+            //и сказать как сделать красивее/лучше... мб через enum можно?
+            for (int i = 0; i < 10; i++)
+            {
+                if (randomShow.Next(0, 2) == 0)
+                    showShape[i] = square.Show();
+                else
+                    showShape[i] = circle.Show();
+
+                Console.WriteLine(showShape[i]);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 10; i++)
+            {
+                if (showShape[i] == "Нарисовать круг")
+                Console.WriteLine(showShape[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ c-courses_class_Shape/ConsoleApplication7/Shape.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication7
+{
+    abstract class Shape
+    {
+        public string color;
+        public int topX;
+        public int topY;
+
+        public Shape(string _color, int _topX, int _topY)
+        {
+            color = _color;
+            topX = _topX;
+            topY = _topY;
+
+        }
+
+        public abstract string Show();
+
+
+    }
+
+    class Square : Shape
+    {
+        public int lourge;
+
+        public Square(string _color, int _topX, int _topY, int _lourge)
+            : base(_color, _topX, _topY)
+        {
+            lourge = _lourge;
+        }
+
+        public override string Show()
+        {
+            return "Нарисовать квадрат";
+        }
+    }
+
+    class Circle : Shape
+    {
+        public int radius;
+
+        public Circle(string _color, int _topX, int _topY, int _radius)
+            : base(_color, _topX, _topY)
+        {
+            radius = _radius;
+        }
+
+        public override string Show()
+        {
+            return "Нарисовать круг";
+        }
+    }
+}
